0bc1218ee7ae68d2c10596a4d0bb9a070b49b4b2
Melissa Astbury <melissajastbury@gmail.com>
2020-07-28 11:32:02 +0100

    implemented 'react-router-dom' to navigate through each container with it's own unique url code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.scss";
 
+import { Route, Switch } from "react-router-dom";
+
 import Home from "./containers/Home/Home";
 import Rooms from "./containers/Rooms/Rooms";
 import SingleRooms from "./containers/SingleRooms/SingleRooms";
@@ -9,11 +11,12 @@ import ErrorPage from "./containers/ErrorPage/ErrorPage";
 const App = () => {
   return (
     <div>
-      <h1>Beach Resort</h1>
-      <Home />
-      <Rooms />
-      <SingleRooms />
-      <ErrorPage />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/rooms/:single" exact component={SingleRooms} />
+        <Route component={ErrorPage} />
+      </Switch>
     </div>
   );
 };
