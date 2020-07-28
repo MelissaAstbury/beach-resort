1f539aacdba85762449e5f7f7b1f9e7aead44575
Melissa Astbury <melissajastbury@gmail.com>
2020-07-28 12:16:58 +0100

    header has now been added to each page and this is also responsive. You can toggle the icon to display the links and hide them

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import "./App.scss";
 
 import { Route, Switch } from "react-router-dom";
 
+import Header from "./components/Header/Header";
 import Home from "./containers/Home/Home";
 import Rooms from "./containers/Rooms/Rooms";
 import SingleRooms from "./containers/SingleRooms/SingleRooms";
 import ErrorPage from "./containers/ErrorPage/ErrorPage";
 
 const App = () => {
+  const routes = (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms" exact component={Rooms} />
+      <Route path="/rooms/:single" exact component={SingleRooms} />
+      <Route component={ErrorPage} />
+    </Switch>
+  );
+
   return (
-    <div>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms" exact component={Rooms} />
-        <Route path="/rooms/:single" exact component={SingleRooms} />
-        <Route component={ErrorPage} />
-      </Switch>
-    </div>
+    <React.Fragment>
+      <Header />
+      {routes}
+    </React.Fragment>
   );
 };
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import logo from "../../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [headerOpened, setHeaderOpened] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Beach Resort" />
+          </Link>
+          <button
+            type="button"
+            className="nav-btn"
+            onClick={() => {
+              setHeaderOpened(!headerOpened);
+            }}
+          >
+            <FaAlignRight className="nav-icon" />
+          </button>
+        </div>
+        <ul className={headerOpened ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
