9c350823e464a511b05fe7863b35759aba738771
Melissa Astbury <melissajastbury@gmail.com>
2020-07-30 19:43:46 +0100

    using context API I have rendered each room in the data file. I have only pulled through the information required on each room. Also I have created a function in my context so that I can filter for 'Featured Rooms' only. This has also been passed to the FeaturedRooms component to be render and is showing in the console

--- src/components/FeaturedRooms/FeaturedRooms.js
+++ src/components/FeaturedRooms/FeaturedRooms.js
@@ -5,9 +5,11 @@ import { RoomContext } from "../../context/RoomContext";
 const FeaturedRooms = () => {
   const { hello } = useContext(RoomContext);
   console.log(hello);
+  const { featuredRooms: rooms } = useContext(RoomContext);
+  console.log(rooms);
   return (
     <div>
-      <p>Featured Rooms</p>
+      <p>Featured Rooms {hello}</p>
     </div>
   );
 };
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -1,16 +1,45 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+
+import items from "../data";
 
 export const RoomContext = createContext();
 
 const RoomProvider = (props) => {
-  // const initialState = JSON.parse(localStorage.getItem("rooms")) || [];
-
-  // const [room, setRoom] = useState(initialState);
   const hello = "hello";
+  const [rooms, setRooms] = useState([]);
+  const [sortedRooms, setSortedRooms] = useState([]);
+  const [featuredRooms, setFeaturedRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //call the formatted data
+  useEffect(() => {
+    //set rooms
+    const rooms = formatData(items);
+    //set the featured rooms
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    setRooms(rooms);
+    setSortedRooms(rooms);
+    setFeaturedRooms(featuredRooms);
+    setLoading(false);
+  }, []);
+
+  //format data is getting all the required fields we want to pull from the originl data
+  const formatData = (items) => {
+    let tempItems = items.map((item) => {
+      const id = item.sys.id;
+      const images = item.fields.images.map((image) => image.fields.file.url);
+
+      const room = { ...item.fields, images: images, id: id };
+      return room;
+    });
+    return tempItems;
+  };
+
   return (
     <RoomContext.Provider
       value={{
         hello,
+        featuredRooms,
       }}
     >
       {props.children}
@@ -18,7 +47,4 @@ const RoomProvider = (props) => {
   );
 };
 
-// const RoomConsumer = RoomContext.Consumer;
-
 export default RoomProvider;
-// export { RoomProvider, RoomContext, RoomConsumer };
