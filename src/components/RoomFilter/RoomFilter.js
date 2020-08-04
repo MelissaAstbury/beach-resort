aa6c269a38aec93bbf9a8df70874ce849dc52962
Melissa Astbury <melissajastbury@gmail.com>
2020-08-04 11:56:15 +0100

    mapped through all the rooms to gather their types. This was then pushed into an array of types which is now presented as a select for the customer to choose from

--- src/components/RoomFilter/RoomFilter.js
+++ src/components/RoomFilter/RoomFilter.js
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { useContext } from "react";
 
-const RoomFilter = () => {
+import { RoomContext } from "../../context/RoomContext";
+import Title from "../Title/Title";
+
+//get all unique values
+const getUniqueValues = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+const RoomFilter = ({ rooms }) => {
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = useContext(RoomContext);
+
+  //get unique types
+  let types = getUniqueValues(rooms, "type");
+  //add all
+  types = ["all", ...types];
+  //map throught JSX
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
   return (
-    <div>
-      <h1>This is the room filter</h1>
-    </div>
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">Room Type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end select type */}
+      </form>
+    </section>
   );
 };
 
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -9,6 +9,15 @@ const RoomProvider = (props) => {
   const [sortedRooms, setSortedRooms] = useState([]);
   const [featuredRooms, setFeaturedRooms] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [type, setType] = useState("all");
+  const [capacity, setCapacity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [minSize, setMinSize] = useState(0);
+  const [maxSize, setMaxSize] = useState(0);
+  const [breakfast, setBreakfast] = useState(false);
+  const [pets, setPets] = useState(false);
 
   //call the formatted data
   useEffect(() => {
@@ -16,10 +25,18 @@ const RoomProvider = (props) => {
     const rooms = formatData(items);
     //set the featured rooms
     const featuredRooms = rooms.filter((room) => room.featured === true);
+    //always show the max price as default
+    const maxPrice = Math.max(...rooms.map((item) => item.price));
+    //always show the max price as default
+    const maxSize = Math.max(...rooms.map((item) => item.size));
+
     setRooms(rooms);
     setSortedRooms(rooms);
     setFeaturedRooms(featuredRooms);
     setLoading(false);
+    setMaxPrice(maxPrice);
+    setMaxSize(maxSize);
+    setPrice(maxPrice);
   }, []);
 
   //format data is getting all the required fields we want to pull from the originl data
@@ -40,13 +57,36 @@ const RoomProvider = (props) => {
     return room;
   };
 
+  const handleChange = (e) => {
+    const type = e.target.type;
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(type, name, value);
+  };
+
+  const filteredRooms = () => {
+    console.log("hello");
+  };
+
   return (
     <RoomContext.Provider
       value={{
+        rooms,
         featuredRooms,
         getRoom,
         loading,
         sortedRooms,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets,
+        handleChange,
+        filteredRooms,
       }}
     >
       {props.children}
